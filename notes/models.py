1da964e1688e5a0da0786d30795feea3db676819
Aswathy TP <aswathy.tp@vuelogix.com>
2025-05-31 15:06:00 +0530

    tag model is created and linked to notes

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Note(models.Model):
     title = models.CharField(max_length=255)
     note = models.TextField()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='notes', null=True)  # Many-to-One
     created_at = models.DateTimeField(auto_now_add=True)  # timestamp created
     updated_at = models.DateTimeField(auto_now=True)      # timestamp updated
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
--- notes/serializers.py
+++ notes/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models import Note
+from .models import Note, Tag
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'title']
+ 
 class NoteSerializer(serializers.ModelSerializer):
+    tag_detail = TagSerializer(source='tag', read_only=True)  # optional
+
     class Meta:
         model = Note
-        fields = '__all__'
+        fields = ['id', 'title', 'note', 'tag_detail', 'created_by', 'created_at', 'updated_at']
         read_only_fields = ['created_by', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        title = validated_data.get('title').strip()
+
+        # Check if a tag with the same title exists
+        tag, created = Tag.objects.get_or_create(title=title)
+
+        # Attach the tag to the note
+        validated_data['tag'] = tag
+
+        # Create the note
+        return Note.objects.create(**validated_data)
