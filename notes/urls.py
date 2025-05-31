2283d81ac875924f1cf23d5f293e4f149bb1119a
Aswathy TP <aswathy.tp@vuelogix.com>
2025-05-31 15:36:46 +0530

    Overview API added

--- notes/urls.py
+++ notes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NoteViewSet
+from .views import NoteViewSet, NotesSummaryAPIView, NoteDetailAPIView
 
 router = DefaultRouter()
 router.register(r'notes', NoteViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('notes/summary/', NotesSummaryAPIView.as_view(), name='notes-summary'),
+    path('notes/<int:pk>/', NoteDetailAPIView.as_view(), name='note-detail'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
 from .models import Note
 from .serializers import NoteSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
@@ -9,3 +12,31 @@ class NoteViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+class NotesSummaryAPIView(generics.GenericAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    def get(self, request, *args, **kwargs):
+        notes = self.get_queryset()
+        total_notes = notes.count()
+
+        # Build list of notes with hyperlink to detail
+        notes_list = [
+            {
+                'id': note.id,
+                'title': note.title,
+                'url': reverse('note-detail', args=[note.id], request=request)
+            }
+            for note in notes
+        ]
+
+        return Response({
+            'total_notes': total_notes,
+            'notes': notes_list
+        })
+    
+
+class NoteDetailAPIView(generics.RetrieveAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
